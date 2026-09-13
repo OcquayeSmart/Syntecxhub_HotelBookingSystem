f772cd7fa9ca98f317237f5b1b8216dd305e8dc0
Ocquaye Smart <snaocquaye@st.knust.edu.gh>
2026-09-13 14:00:26 -0100

    adds rooms fields and constructors

--- src/Rooms.java
+++ src/Rooms.java
@@ -1,2 +1,38 @@
 public class Rooms {
+    int roomNumber;
+    RoomType type;
+    double roomPrice;
+    boolean isAvailable;
+
+    public Rooms(int roomNumber, RoomType type, double rooMPrice, boolean isAvailable) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.roomPrice = rooMPrice;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public double getRoomPrice() {
+        return roomPrice;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomNumber=" + roomNumber +
+                ", type=" + type +
+                ", rooMPrice=" + roomPrice +
+                '}';
+    }
 }
